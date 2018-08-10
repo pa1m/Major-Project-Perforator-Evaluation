cba18b87e1bdde70ff32a313e90133918bb29f83
pa1m <42262878+pa1m@users.noreply.github.com>
2018-08-10 19:31:18 +0530

    Add files via upload

---
+++ Spec.py
@@ -0,0 +1,65 @@
+import scipy.io.wavfile as wav
+import numpy as np
+from matplotlib import pyplot as plot
+
+fs, data = wav.read('sample.wav')
+sf = int(fs)
+left=[]
+for i in range (0,len(data)):
+	left.insert(i,int(data[i][0]))
+	
+#left1 = int(left)
+right=[]
+for i in range (0,len(data)):
+	right.insert(i,int(data[i][1]))
+
+time = 0.025
+numFramesNeeded=int(np.ceil(fs*time))
+
+overlap = int(np.ceil(fs*0.015))
+
+# Plot the signal read from wav file
+
+plot.subplot(411)
+
+plot.title('Spectrogram of a wav file')
+
+ 
+
+plot.plot(left)
+
+plot.xlabel('Sample')
+
+plot.ylabel('Amplitude')
+
+
+
+
+plot.subplot(412)
+
+plot.specgram(x=left,NFFT=numFramesNeeded,  Fs=sf, noverlap=overlap)
+
+plot.xlabel('Time')
+
+plot.ylabel('Frequency')
+
+
+
+plot.subplot(413)
+
+plot.specgram(x=right,NFFT=numFramesNeeded,  Fs=sf, noverlap=overlap)
+
+plot.xlabel('Time')
+
+plot.ylabel('Frequency')
+
+plot.subplot(414)
+plot.plot(right)
+
+plot.xlabel('Sample')
+
+plot.ylabel('Amplitude')
+
+
+
+plot.show()
