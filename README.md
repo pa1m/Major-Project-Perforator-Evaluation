8ec057a5b99eb5832e2681e7a2cd54c9273e2d57
pa1m <pavan.mlv@gmail.com>
2018-10-26 12:41:30 +0530

    Added Data and stereo,spectrogram code

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Major-Project-Perforator-Evaluation
 Sound processing of Hand Held Doppler
-1. Spectrogram
+1. Spectrogram (Online code and Scipy library)
 2. Auto Correlation
+3. Stereo to check different channel data seperately
--- autocorrelation.py
+++ autocorrelation.py
@@ -4,59 +4,90 @@ import peakutils
 import scipy.signal
 from matplotlib import pyplot as plot
 import matplotlib.mlab as mlab
+from scipy.signal import butter, lfilter
 
 
-fs, data = wav.read('sample.wav')
+def butter_lowpass(cutOff, fs, order):
+    nyq = 0.5 * fs
+    normalCutoff = cutOff / nyq
+    b, a = butter(order, normalCutoff, btype='low', analog = True)
+    return b, a
+
+fs, data = wav.read('rec1.wav')
 sf = int(fs)
 left=[]
+right=[]
 for i in range (0,len(data)):
 	left.insert(i,int(data[i][0]))
+	right.insert(i,int(data[i][1]))
+
+
 
 time = 0.025
 numFramesNeeded=int(np.ceil(fs*time))
 
 overlap = int(np.ceil(fs*0.015))
 
-print(fs)
-print(np.ceil(fs*time))
-print ((numFramesNeeded))
-print(len(left))
 
 frames = 0
 count = 0
 data_acorr = []
-shiftval = 250000
+shiftval = 150000
 
 index_peak1 = []
 amplitude_peak1 = []
 index_peak2 = []
 amplitude_peak2 = []
 
-specdata = []
+left = np.array(left,dtype=np.float)
 
-while ( count < 1):
-	
-	new_data = left[shiftval+frames:shiftval+frames+numFramesNeeded]
+    
+#w_left = scipy.signal.wiener(left,mysize = 10)
+
+#scipy.io.wavfile.write('rec1wiener.wav',fs,w_left)
+
+specdata = []
+wienerdata = []
+
+while ( count < 200):
+
+	#new_data = w_left[shiftval+frames:shiftval+frames+numFramesNeeded]
+	new_data = low_left[shiftval+frames:shiftval+frames+numFramesNeeded]
 	new_data = np.array(new_data,dtype=np.float)
 
 	specdata.extend(new_data)
-	
-	temp_acorr = plot.acorr(new_data,maxlags=500)
+
+	wiener_data = scipy.signal.wiener(new_data,mysize = 1199)
+
+	wienerdata.extend(wiener_data)
+
+	#temp_acorr = plot.acorr(new_data,maxlags=1199)
+	temp_acorr = plot.acorr(wiener_data,maxlags=1199)
 	temp_acorr = np.array(temp_acorr)[1]
 
 	frames = frames + numFramesNeeded
 	count = count +1
 
-	half_temp_acorr = temp_acorr[500:]
-	indexes = peakutils.peak.indexes(np.array(half_temp_acorr),thres=-0.2, min_dist=10)
-	
+	half_temp_acorr = temp_acorr[1199:]
+	indexes = peakutils.peak.indexes(np.array(half_temp_acorr),thres=0.0, min_dist=10)
+
 	index2 = indexes[0:2]
-	
-	index_peak1.append(index2[0])
-	amplitude_peak1.append(half_temp_acorr[index2[0]])
-	
-	index_peak2.append(index2[1])
-	amplitude_peak2.append(half_temp_acorr[index2[1]])
+
+	if(index2.size):
+		index_peak1.append(index2[0])
+		amplitude_peak1.append(half_temp_acorr[index2[0]])
+		
+	else:
+		index_peak1.append(500)
+		amplitude_peak1.append(0)
+
+
+	#index_peak2.append(index2[1])
+	#amplitude_peak2.append(half_temp_acorr[index2[1]])
+m = min(index_peak1)
+shift = (fs*1.0)/110
+v = (shift * 1540.0) / (3.6*10000)
+print(v)
 	
 	
 	
@@ -73,45 +104,46 @@ while ( count < 1):
 #print(specdata)
 
 
-print(max(index_peak1))
-print(min(index_peak1))
+#print(max(index_peak1))
+
+
 
 plot.clf()
-#plot.subplot(211)
+plot.subplot(411)
 #plot.title('Frame Difference b/w Zeroth Peak and First peak')
-plot.plot(temp_acorr)
+plot.plot(wienerdata)
 #plot.xlabel('Frames')
 #plot.ylabel('Frame Difference for First Peak')
 
-#plot.subplot(212)
-#plot.title('Amplitude of peak for First Peak')
-#plot.plot(amplitude_peak1)
-#plot.xlabel('Frames')
-#plot.ylabel('Amplitude of peak for First Peak')
+#plot.subplot(412)
+##plot.title('Amplitude of peak for First Peak')
+#plot.plot(specdata)
+##plot.xlabel('Frames')
+##plot.ylabel('Amplitude of peak for First Peak')
 
 
-##plot.subplot(413)
+plot.subplot(412)
 
-##Pxx, freq, t = mlab.specgram(x=specdata,NFFT=numFramesNeeded,  Fs=sf, noverlap=overlap)
-#plot.subplot(413)
-#plot.specgram(x=left,NFFT=numFramesNeeded,  Fs=sf, noverlap=overlap, mode='psd')
-##ha.set_yscale('log')
+#Pxx, freq, t = mlab.specgram(x=specdata,NFFT=numFramesNeeded,  Fs=sf, noverlap=overlap)
+plot.specgram(x=specdata,NFFT=numFramesNeeded,  Fs=sf, noverlap=overlap, mode='psd')
+plot.ylim((0,2000))
+#ha.set_yscale('log')
 
 
 ##plot.xlabel('Time')
 
 ##plot.ylabel('Frequency')
 
-##plot.subplot(413)
+plot.subplot(413)
 ##plot.title('Frame Difference b/w Zeroth Peak and Second peak')
-##plot.plot(index_peak2)
+plot.plot(index_peak1)
 
 ##plot.xlabel('Frames')
 ##plot.ylabel('Frame Difference for Second Peak')
 
-#plot.subplot(414)
+plot.subplot(414)
 ##plot.title('Amplitude of peak for Second Peak')
-#plot.plot(specdata)
+plot.plot(temp_acorr)
 
 ##plot.xlabel('Frames')
 ##plot.ylabel('Amplitude of peak for Second Peak')
